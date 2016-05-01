7bf0b5cba724e6543286cbed6a2acd52c093df04
Marcleroni <marcfettback@Marc-Fettbacks-MacBook-Pro.local>
2016-05-01 23:15:18 +0200

    CharacterController and DoubleJump
    Walking, Jumping and DoubleJump im CharacterController Script eingefügt.

---
+++ mutor2016/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	Vector2 velocity;
+	public float smoothTimeX;
+	public float smoothTimeY;
+
+	public GameObject player;
+
+	public bool bounds;
+	public Vector3 min;
+	public Vector3 max;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+		transform.position = new Vector3 (posX, posY, transform.position.z);
+
+		if (bounds) {
+
+			transform.position = new Vector3 (Mathf.Clamp(transform.position.x, min.x, max.x), Mathf.Clamp(transform.position.y, min.y, max.y), Mathf.Clamp(transform.position.z, min.z, max.z));
+
+		}
+
+	}
+}
--- mutor2016/Assets/Scripts/PlayerController.cs
+++ mutor2016/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,77 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	public float maxSpeed = 1.6f;
+	public bool facingRight = true;
+	Rigidbody2D rb;
+
+	bool grounded = false;
+	bool grounddist = false;
+	public Transform groundCheck;
+	public Transform distCheck;
+	float groundRadius = 0.10f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 200;
+
+	bool doubleJump = true;						//doubleJump Variable
+	public bool doubleJumpEnabled = false;		//doubleJump ON/OFF
+
+
+
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
+
+	void FixedUpdate () {
+
+		float move = Input.GetAxis ("Horizontal");						//normales Movement
+		rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y); 
+
+		if (move > 0 && !facingRight)									//Flip um Y-Achse
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+		if (grounded && doubleJumpEnabled)								//doubleJump reset
+			doubleJump = false;
+
+	}
+
+
+
 	void Update () {
-	
+
+		if((grounded || !doubleJump) && Input.GetButtonDown ("Jump")) {	//doubleJump or Parameter
+
+			//AudioSource.PlayClipAtPoint(jump,this.GetComponent<Transform>().position, jumpVolume);
+
+			//anim.SetBool ("Ground", false);
+			rb.velocity = new Vector2 (rb.velocity.x, 0);
+			rb.AddForce(new Vector2 (0, jumpForce));
+
+
+			grounddist = Physics2D.Linecast(groundCheck.position, distCheck.position, whatIsGround); //Smooth landing animation
+			//anim.SetBool ("Dist", grounddist);
+
+
+			if (!doubleJump && !grounded)	//doubleJump
+				doubleJump = true;			//doubleJump
+
+		}
+
+
 	}
+
+
+	public void Flip () {
+
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+	}
+
 }
