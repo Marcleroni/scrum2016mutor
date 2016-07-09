36ca9d406e4909077d286fcd1c44ee708e9d0752
Marcleroni <marc@fettback.com>
2016-07-09 12:33:19 +0200

    Level2 finish

---
+++ mutor2016/Assets/Scripts/SideScrollMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideScrollMenu : MonoBehaviour {
+
+	public float speed = 1f;
+	public Vector3 resetEnd;
+	public Vector3 resetStart;
+	Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tf.Translate(Vector3.left*speed);
+		if (tf.position.x < -5.996) {
+			tf.position = new Vector3(5.996f,-1.805f,0f);
+		}
+	}
+}
